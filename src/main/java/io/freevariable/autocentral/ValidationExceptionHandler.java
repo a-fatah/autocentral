888c2d700bf35a0f3a1f62e71041630e15e0b594
a-fatah <abdulfatah66@gmail.com>
2023-03-22 19:43:37 +0100

    handle validation errors for REST API

---
+++ src/main/java/io/freevariable/autocentral/ValidationExceptionHandler.java
@@ -0,0 +1,45 @@
+package io.freevariable.autocentral;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleValidationException(ConstraintViolationException ex, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            errors.add(violation.getMessage());
+        }
+        return ResponseEntity.badRequest()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorDetails("Validation failed", errors));
+    }
+}
+
+
+@Getter
+@Setter
+class ErrorDetails {
+    private String message;
+    private List<String> errors;
+
+    public ErrorDetails(String message, List<String> errors) {
+        this.message = message;
+        this.errors = errors;
+    }
+
+}
