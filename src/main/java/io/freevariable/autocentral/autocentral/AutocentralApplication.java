3696c2fe894006befef213afbc2b2aa6ac88eee3
a-fatah <abdulfatah66@gmail.com>
2023-03-22 15:54:42 +0100

    initial commit

---
+++ src/main/java/io/freevariable/autocentral/autocentral/AutocentralApplication.java
@@ -0,0 +1,13 @@
+package io.freevariable.autocentral.autocentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/freevariable/autocentral/autocentral/AutocentralApplicationTests.java
@@ -0,0 +1,13 @@
+package io.freevariable.autocentral.autocentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
